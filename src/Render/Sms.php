c902c383ff26fee94ac26e1d9337d55726a64a15
Joe Simpson <headbangerkenny@googlemail.com>
2015-09-25 17:31:54 +0100

    Render images inside of messaging themes. iOS 6 theme is messed up however

--- src/Render/Sms.php
+++ src/Render/Sms.php
@@ -17,6 +17,7 @@ class Sms extends BaseRenderer {
 
             $from = substr($line, 0, stripos($line, ":"));
             $msg = substr($line, stripos($line, ":")+1);
+
             if(!$classes[$from]){
                 $upto += 1;
                 $classes[$from] = $upto;
@@ -26,12 +27,22 @@ class Sms extends BaseRenderer {
             }
             $class = $classes[$from];
 
-            $lines[] = array(
-                "from" => $from,
-                "message" => $msg,
-                "photo" => $page->get_image_url($yaml[$from]),
-                "class" => $class
-            );
+            $matches = array();
+            if(preg_match('/!\[((?:\[[^\]]*\]|[^\[\]]|\](?=[^\[]*\]))*)\]\(\s*<?([\s\S]*?)>?(?:\s+[\'"]([\s\S]*?)[\'"])?\s*\)/', $msg, $matches) > 0){
+                $lines[] = array(
+                    "from" => $from,
+                    "image" => $page->get_image_url($matches[2]),
+                    "photo" => $page->get_image_url($yaml[$from]),
+                    "class" => $class
+                );
+            } else{
+                $lines[] = array(
+                    "from" => $from,
+                    "message" => $msg,
+                    "photo" => $page->get_image_url($yaml[$from]),
+                    "class" => $class
+                );
+            }
         }
 
         // Get styles
